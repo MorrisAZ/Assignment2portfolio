1221c5893b65dbe6be2a4eb353d443cc01b2e0ce
Morris Zuniga <mzuni04@my.centennialcollege.ca>
2021-10-22 23:40:59 -0400

    updated URI to work mongo db atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://morris:LO6g25b5dbMaC0Yu@mongodbserver.du7g3.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
